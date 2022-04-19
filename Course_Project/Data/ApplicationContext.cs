4bf45a74fc3532ebfdaceea3bc6ecda22810d604
VladislavSorochinskiy <sorochinskiy02@gmail.com>
2022-04-19 11:36:46 +0300

    Add project files.

---
+++ Course_Project/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course_Project.Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Course_Project/Data/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Course_Project.Data
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
